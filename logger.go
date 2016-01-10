1955018fdaca834621de2f757e76a6c05d0412ab
Rodrigo Valente <rodrigoavrav@gmail.com>
2016-01-09 23:13:40 -0300

    INITIAL COMMIT

---
+++ logger.go
@@ -0,0 +1,30 @@
+package logger
+
+import (
+	"log"
+	"net/http"
+)
+
+type LogRecord struct {
+	http.ResponseWriter
+	Status int
+}
+
+func (logger *LogRecord) Write(data []byte) (int, error) {
+	return logger.ResponseWriter.Write(data)
+}
+
+func WrapHandler(handler http.Handler) http.HandleFunc {
+	return func(writer http.ResponseWriter, request *htpp.Request) {
+        record := &LogRecord{
+            ResponseWriter: writer
+        }
+
+        handler.ServeHTTP(record, handler)
+        log.Println("Bad Request ", record.Status)
+
+        if record.Status == http.StatusBadRequest {
+            log.Println("Bad Request ", handler)
+        }
+	}
+}
